ecf62689dc1eab01996f3dc9387bc7bb636383e5
Abhishek Guha <abhi.workspace@gmail.com>
2021-08-08 22:59:24 +0200

    Some logging nicety

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
-const version string = "gomac"
+const (
+	version string = "gomac"
+	port    string = ":8080"
+)
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
@@ -23,9 +30,38 @@ func newRouter() *mux.Router {
 }
 
 func main() {
+	l := log.New(os.Stdout, "gomac -> ", log.LstdFlags)
 	r := newRouter()
 
-	http.ListenAndServe(":8000", r)
+	s := &http.Server{
+		Addr:         port,
+		Handler:      r,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 2 * time.Second,
+	}
+
+	go func() {
+		l.Println("Starting server on port", port)
+
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	log.Printf("Received %s, gracefully shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+
+	defer cancel()
+	s.Shutdown(tc)
 }
 
 func getTemperature(object string) string {
